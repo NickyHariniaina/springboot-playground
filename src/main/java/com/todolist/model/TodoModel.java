f9ef3b52f351360e7c30bb1213282dcd4c81e8b5
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-11 13:56:23 +0300

    feat: update todo entity col

--- src/main/java/com/todolist/model/TodoModel.java
+++ src/main/java/com/todolist/model/TodoModel.java
@@ -1,27 +1,82 @@
 package com.todolist.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 /** TodoModel */
 @Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "todo")
 public class TodoModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
+  @Column(nullable = false, unique = true, length = 50)
   private String title;
-  private String description;
-  private boolean completed;
-  private int userId;
-}
 
+  @Column(nullable = true, unique = false)
+  private String description;
+
+  private boolean completed;
+
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, targetEntity = UserModel.class)
+  private int userId;
+
+  public TodoModel(String title, String description, boolean completed, int userId) {
+    this.title = title;
+    this.description = description;
+    this.completed = completed;
+    this.userId = userId;
+  }
+
+  public TodoModel() {}
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+}
--- src/main/java/com/todolist/model/UserModel.java
+++ src/main/java/com/todolist/model/UserModel.java
@@ -1,28 +1,63 @@
 package com.todolist.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Table;
 
 @Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
+@Table(name = "user")
 public class UserModel {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
+  @Column(nullable = false, unique = true, length = 100)
   private String username;
+  @Column(nullable = false, unique = true)
   private String email;
   private String password;
 
+  public UserModel(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
 
+  public UserModel() {}
 
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
