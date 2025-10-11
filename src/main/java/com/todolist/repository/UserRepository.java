9f9a4e48d4e9887c285e7fa8e3410e9e88a61070
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-11 14:35:05 +0300

    feat: add repository custom method

--- src/main/java/com/todolist/repository/UserRepository.java
+++ src/main/java/com/todolist/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.todolist.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,11 @@ import com.todolist.model.UserModel;
  */
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
+  List<UserModel> findAllByUsernameContaining(String stringSlice);
+  List<UserModel> findAllByEmailStartingWith(String stringSlice);
+  List<UserModel> findAllByEmailLike(String stringSlice);
+  Optional<UserModel> findByUsername(String username);
+  Optional<UserModel> findByEmail(String email);
+  boolean existsByEmail(String email);
+  boolean existsByUsername(String username);
 }
