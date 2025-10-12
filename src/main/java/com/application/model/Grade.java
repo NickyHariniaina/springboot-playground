710a3bbc5fe85f680ce0348157740a7fb4641e52
NickyHariniaina <nickyharini1na@gmail.com>
2025-10-12 14:54:44 +0300

    feat: add group model

---
+++ src/main/java/com/application/model/Grade.java
@@ -0,0 +1,57 @@
+package com.application.model;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Grade
+ */
+@Entity
+@Table(name = "\"grade\"")
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Grade {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  @ManyToOne
+  @JoinColumn(name = "student_id", updatable = false)
+  private User user;
+
+  @Column(updatable = false)
+  @CreationTimestamp
+  @EqualsAndHashCode.Exclude
+  private Instant creationDatetime;
+
+}
+
+
+
+
+
+
+
+
+
